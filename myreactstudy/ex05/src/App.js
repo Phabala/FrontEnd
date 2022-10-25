fa6f69d9c7c2146959858641151adbc632cd3a2f
Kim Gihong <kihong2424@gmail.com>
2022-10-25 12:13:00 +0900

    리액트 수업 로그인(useRef, useNavigate, useLocation)

--- myreactstudy/ex05/src/App.js
+++ myreactstudy/ex05/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Routes, Route} from "react-router-dom";
 import Join from './component/Join'
 import Login from './component/Login'
+import Home from './component/Home'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Join/>}/>
       <Route path='/Login' element={<Login/>}/>
+      <Route path='/Home' element={<Home/>}/>
     </Routes>
     </>
   );
